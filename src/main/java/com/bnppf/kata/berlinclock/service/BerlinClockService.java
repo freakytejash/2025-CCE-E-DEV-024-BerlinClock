081eb722889a710d1e9fe44ee6cb2c44fba31de1
Tejash Mehta <tejash.mehta@ext.belgiantrain.be>
2025-10-26 15:14:00 +0100

    test: add tests for five-hour row calculation
    - Test 0 hours returns all lamps off
    - Test 5, 10, 15, 20+ hours
    - Each red lamp represents 5 hours

---
+++ src/main/java/com/bnppf/kata/berlinclock/service/BerlinClockService.java
@@ -0,0 +1,4 @@
+package com.bnppf.kata.berlinclock.service;
+
+public class BerlinClockService {
+}
--- src/test/java/com/bnppf/kata/berlinclock/service/BerlinClockServiceTest.java
+++ src/test/java/com/bnppf/kata/berlinclock/service/BerlinClockServiceTest.java
@@ -24,4 +24,30 @@ class BerlinClockServiceTest {
         assertEquals("O", service.getSecondsLamp(1));
         assertEquals("O", service.getSecondsLamp(59));
     }
+
+    @Test
+    void shouldReturnAllOffLampsForZeroHours() {
+        assertEquals("OOOO", service.getFiveHourRow(0));
+    }
+
+    @Test
+    void shouldReturnOneRedLampForFiveHours() {
+        assertEquals("ROOO", service.getFiveHourRow(5));
+    }
+
+    @Test
+    void shouldReturnTwoRedLampsForTenHours() {
+        assertEquals("RROO", service.getFiveHourRow(10));
+    }
+
+    @Test
+    void shouldReturnThreeRedLampsForFifteenHours() {
+        assertEquals("RRRO", service.getFiveHourRow(15));
+    }
+
+    @Test
+    void shouldReturnFourRedLampsForTwentyHours() {
+        assertEquals("RRRR", service.getFiveHourRow(20));
+        assertEquals("RRRR", service.getFiveHourRow(23));
+    }
 }
