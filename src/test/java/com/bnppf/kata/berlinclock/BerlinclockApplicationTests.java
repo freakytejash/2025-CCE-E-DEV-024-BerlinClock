e50ae2157446239f0a7b92d9729ab8e2b765bf17
Tejash Mehta <tejash.mehta@ext.belgiantrain.be>
2025-10-26 14:53:08 +0100

    chore: initialize Spring Boot project with Maven
    - Add Spring Web dependency
    - Configure Java 17
    - Set up Maven wrapper

---
+++ src/test/java/com/bnppf/kata/berlinclock/BerlinclockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnppf.kata.berlinclock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BerlinclockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
